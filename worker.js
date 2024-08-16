051431b8e391ef98bc9557cf57ac8dc5a3aeaae4
boraayhan <boraayhanwebsite@gmail.com>
2024-08-16 07:39:06 -0600

    Made background color more dynamic

--- worker.js
+++ worker.js
@@ -1,6 +1,9 @@
-let bannedStrings = ["reddit.com", "instagram.com","youtube.com"]
-html = '<div style="text-align:center; font-size: 1.7vw; font-family:verdana"><br/><br/><b>CAUGHT YOU LACKING!! 🚨🚨🚨🚨 😠😠😠</b> <br/><br/><b>LOCK IN and get that academic comeback! 🗣🗣🗣🔥🔥🔥</b></div>'
+console.log("LockIN Website Block active!");
+let bannedStrings = ["reddit.com", "instagram.com", "youtube.com"];
+
+html = '<div style="text-align:center; font-size: 1.7vw; font-family:verdana "><br/><br/><b style="color:white">CAUGHT YOU LACKING!! 🚨🚨🚨🚨 😠😠😠</b> <br/><br/><b style="color:white">LOCK IN and get that academic comeback! 🗣🗣🗣🔥🔥🔥</b></div>'
 function display() {
+    document.body.style.background = '#282928';
     document.body.innerHTML = html;
 }
 
@@ -9,10 +12,10 @@ function Block(site, endDate, pass) {
     this.endDate = endDate;
     this.pass = pass;
 }
-
-console.log("Real");
-if (!window.location.href.includes('chrome://')) {
-    if (bannedStrings.some(site => window.location.href.includes(site))) {
-        display();
+window.addEventListener("load", function () {
+    if (!window.location.href.includes('chrome://')) {
+        if (bannedStrings.some(site => window.location.href.includes(site))) {
+            display();
+        }
     }
-}
+});
